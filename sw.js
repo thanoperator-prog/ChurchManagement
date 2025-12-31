e25fea97d2081c8d2ffb018019e9d251435902ea
thanoperator-prog <thanoperator@gmail.com>
2025-12-31 21:08:33 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
 const CACHE_NAME = 'ewm-app-v1';
-const ASSETS = [
+const ASSETS_TO_CACHE = [
     './church_manager.html',
     './manifest.json',
     './icon-512.png',
+    // External Libraries (Caching these ensures the app loads even if CDNs are slow)
     'https://cdn.tailwindcss.com',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
     'https://unpkg.com/react@18/umd/react.development.js',
@@ -13,16 +14,43 @@ const ASSETS = [
     'https://unpkg.com/@babel/standalone/babel.min.js'
 ];
 
+// Install Event - Caches assets
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then((cache) => cache.addAll(ASSETS))
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
     );
 });
 
+// Fetch Event - Serves from cache if available, otherwise fetches from network
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
-            .then((response) => response || fetch(event.request))
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// Activate Event - Cleans up old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
